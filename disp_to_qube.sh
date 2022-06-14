157a63179915db397a79e050b8290107f8d08f99
unman <unman@thirdeyesecurity.org>
2022-06-14 13:02:16 +0000

    Convert disposables to AppVM - handle libreoffice

--- disp_to_qube.sh
+++ disp_to_qube.sh
@@ -8,6 +8,10 @@ fi
 WM_CLASS=$(xprop WM_CLASS -id $ID |cut -f2 -d, |tr -d '"' )
 IFS=':' read QUBE APP <<< $WM_CLASS
 app=$(echo $APP | tr '[:upper:]' '[:lower:]')
+case $app in
+  libreoffice-startcenter)
+    app="libreoffice --nologo"
+esac
 qls=$(qvm-ls -O NAME,CLASS,TEMPLATE,LABEL,NETVM $QUBE --raw-data)
 IFS='|' read NAME CLASS TEMPLATE LABEL NETVM QUBE APP <<< $qls
 if [ $CLASS != "DispVM" ];then
@@ -16,11 +20,12 @@ This is not a disposable"
   exit
 else
   dispvm=$(qvm-prefs $NAME default_dispvm )
-  notify-send "Pausing dispvm and preparing replacement AppVM"
+  notify-send "Pausing disposable and preparing replacement AppVM"
   qvm-pause $NAME
   real_template=`qvm-prefs $TEMPLATE template`
   new_qube=`echo $NAME |sed s/disp/qube/ `
-  if [ qvm-check $new_qube ]; then
+  qvm-check --quiet $new_qube 2>/dev/null
+  if [ $? -eq 0 ]; then
     notify-send "$new_qube already exists"
     qvm-unpause $NAME
     exit
@@ -28,8 +33,8 @@ else
   qvm-create $new_qube -t $real_template --property default_dispvm=$dispvm --property netvm=$NETVM -l purple
   cd /dev/qubes_dom0
   sudo dd if=$(readlink /dev/qubes_dom0/vm-$NAME-private-snap) of=$(readlink /dev/qubes_dom0/vm-$new_qube-private) conv=sparse
-  qvm-unpause $NAME
-  qvm-kill $NAME
   notify-send "Starting replacement AppVM"
-  qvm-run $new_qube $app
+  qvm-kill $NAME
+  qvm-run $new_qube 'find -name .lock* -delete'
+  qvm-run $new_qube $app &
 fi
