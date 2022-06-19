1a7d4ac075399db885b3cadea6425381d262fdb4
unman <unman@thirdeyesecurity.org>
2022-06-19 14:42:47 +0000

    Allow running script from dom0

--- disp_to_qube.sh
+++ disp_to_qube.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/bash
-ID=`xdotool getwindowfocus`
+if [ $# -eq 1 ];then
+  qvm-check --quiet $1 2>/dev/null
+  if [ $? -ne 0 ]; then
+    notify-send "No qube $1 "
+    exit
+  else
+    ID=`xdotool selectwindow`
+    if [ "$(xprop _QUBES_VMNAME -id $ID|cut -f2 -d\")" != "$1" ]; then
+      notify-send "Window mismatch"
+      exit
+    fi
+  fi
+else
+  ID=`xdotool getwindowfocus`
+fi
 QUBE=$(xprop _QUBES_VMNAME -id $ID|cut -f2 -d\" )
 if [[ "$QUBE" == "_QUBES_VMNAME:  not found." ]]; then
   notify-send "That is a dom0 window"
@@ -30,11 +44,12 @@ else
     qvm-unpause $NAME
     exit
   fi
-  qvm-create $new_qube -t $real_template --property default_dispvm=$dispvm --property netvm=$NETVM -l purple
+  qvm-create $new_qube -t $real_template --property default_dispvm=$dispvm --property netvm=$NETVM -l $LABEL
   cd /dev/qubes_dom0
   sudo dd if=$(readlink /dev/qubes_dom0/vm-$NAME-private-snap) of=$(readlink /dev/qubes_dom0/vm-$new_qube-private) conv=sparse
   notify-send "Starting replacement AppVM"
+  qvm-unpause $NAME
   qvm-kill $NAME
   qvm-run $new_qube 'find -name .lock* -delete'
-  qvm-run $new_qube $app &
+  qvm-run $new_qube "$app" &
 fi
